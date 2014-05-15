d8a96fdc2348d80a15702d864af317e9e076b75d
puhrome <phirom.yim@fullsail.edu>
2014-05-15 15:15:14 -0400

    Strings and Booleans
    Examples of the difference between strings and booleans in JS using
    WebStorm

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/**
+ * Variables of Different Types, Numbers Booleans and Strings Created by Phirom Yim on 5/15/14.
+ */
+
+var age = 38; //number variable
+
+var isStudent = false;//boolena variable
+
+var kermit = "my friend"; //needs to know the different between variables or the string
+//quotes signals the string
+//if you use single quotes ' be careful of words with with apostrophe ie don't
+//it can confuse the machine
+//you can use escape operate \ to ignore the code ie don\'t
+//functions to tell machine to treat the apostrophe as part of the string not end of string
+
+//-------\n is a new line character------- adding a line break in code
+//popular in many diffent languages
+
+var firstName = "Phirom";//js needs quote to know the beginning or end of string
